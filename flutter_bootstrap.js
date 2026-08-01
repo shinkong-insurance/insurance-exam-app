39e2539fccbb37c6280ec6e103eb42cebe145c94
HUNGSHUOTSUNG <bookishseven@gmail.com>
2026-08-01 20:07:36 +0800

    deploy: web v22 - fix mock exam pdf crash

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2924021234" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2891007602" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
